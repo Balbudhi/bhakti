435bfb787de0766b531fbba99d6c4ccbf837bfe0
Eeshan <eeshan@mit.edu>
2026-08-20 11:14:08 -0400

    Site: launch dedicated Bhakti song reader

---
+++ data/songs.js
@@ -0,0 +1,26 @@
+window.BHAKTI_SONGS = [
+  {
+    slug: "duniya-de-dukhan",
+    title: "Duniyā De Dukhāṅ Menū Baṛā Hī Satāyā Mā",
+    subtitle: "Jai Mātā Dī",
+    credit: "Suresh Ji · Shri Mata Vaishno Devi Bhawan",
+    language: "Punjabi",
+    tags: ["Mātā", "Punjabi"]
+  },
+  {
+    slug: "jhoothe-jag-ne",
+    title: "Jhūṭhe Jag Ne Liyā Mainū Lūṭ Mā",
+    subtitle: "Jai Mātā Dī",
+    credit: "Maninder Ji · Shri Mata Vaishno Devi Bhawan",
+    language: "Punjabi",
+    tags: ["Mātā", "Punjabi"]
+  },
+  {
+    slug: "koi-hor-nahi",
+    title: "Koī Hor Nahī Hai Merā",
+    subtitle: "Jai Mātā Dī",
+    credit: "Devotional song reader",
+    language: "Punjabi",
+    tags: ["Mātā", "Punjabi"]
+  }
+];
